ba14fc1380b087ef226ce97e54ec2dd37a07cde7
jirichmel <jirichmel@protonmail.com>
2020-05-07 20:45:46 +0200

    pymatgen package integrated.

--- ngram/ngram.py
+++ ngram/ngram.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Version info:
+# 6.5.2020 -- deprecation custom function in facor of pymatgen package.
+
 import numpy as np
 import pandas as pd
 import networkx as nx
+
+from pymatgen import core as core
 
 #
 # Crystal graph representation which converts 
@@ -48,98 +53,95 @@ def xyzcoord(lattice_matrix,frac_vector):
 #we must account for the fact that we have data only of one unit cell. 
 #But the distance of the atom i from the atom j can be the smallest
 #if we compare it with the atom j in the neighboring cell.
-#Therefore it is necessary to test all the neighboring cells as well.
-#That means to do 9 comparisons in total (8 neighboring cells and the
-#actual cell we are in).
+#Therefore it is necessary to test a beforehand unknown amount of
+#neighboring cells because sometimes, there are problems with
+#(e.g. spacegroup 227) the choice of the nearest instance of an atom.
 
-def length(vector):
-    """Calculate the euclidean distance of a vector.
-    
-    Keyword arguments:
-    vector -- input numpy vector
-    """
-    return np.linalg.norm(vector,ord=2)
+def init_material(df_frac, df_latt, ajdi, rsn):
+    """Initiate the IStructure class of pymatgen using the data.
+    Returns an instance of IStructure for the given material data.
 
-def minimal_distance(df_frac, df_lattice, ajdi, relaxation_step_number):
-    """Get the shortest distances between atoms of the given material id and relaxation_step_number.
-    Outputs a matrix of distances and an array of species.
-    
     Keyword arguments:
     df_frac -- dataframe of fractional coordinates of atoms
-    df_lattice -- dataframe of lattice vectors
+    df_latt -- dataframe of lattice vectors
     ajdi -- id of the material
-    relaxation_step_number -- self explanatory
+    rsn -- relaxation step number of the material
     """
-    # get the slice of the atoms for the given id and relarelaxation_step_number
-    atoms = df_frac.loc[(df_frac["id"]==ajdi) & (df_frac["relaxation_step_number"]==relaxation_step_number)]
-    
-    # Species vector:
-    species_vector = atoms["species"].to_numpy()
-    
-    number_of_total_atoms = len(atoms.index.values)
-    
-    # A symmetrical matrix which holds the distances of i-th and j-th atoms, the diagonal is zeros.
-    minimal_distance_matrix = np.zeros((number_of_total_atoms,number_of_total_atoms))
-    
-    #the lattice vector matrix:
-    A = lattmat(df_lattice,ajdi,relaxation_step_number)
-    
-    for i in range(number_of_total_atoms):
-        for j in range(i):
-            
-            # The difference of fractional vectors:
-            ij = atoms.loc[atoms.index.values[i],["L1","L2","L3"]].to_numpy() - atoms.loc[atoms.index.values[j],["L1","L2","L3"]].to_numpy()
-            
-            # Initialize the dummy variables for this iteration:
-            minimal_distance = np.inf # minimal distance between two atoms
-            
-                # Seek through the neighboring unit cells:
-            for k in range(-1, 2):
-                for l in range(-1, 2):
-                    for m in range(-1, 2):
+    lattice = np.transpose(lattmat(df_latt,ajdi,rsn)) # pymatgen compatible lattice vector
+    species = df_frac[(df_frac["id"]==ajdi) & (df_frac["relaxation_step_number"]==rsn)].species.values
+    coords = df_frac[(df_frac["id"]==ajdi) & (df_frac["relaxation_step_number"]==rsn)][df_frac.columns[-3:]].values
 
-                        # difference of ij the neighboring cell shift:
-                        r = ij + np.array([k,l,m])
+    material = core.IStructure(lattice=lattice, species=species, coords=coords, to_unit_cell=True, coords_are_cartesian=False)
+    return material
 
-                        # euclidean distance of the vectors which are recalculated back:
-                        R = xyzcoord(A,r)
-                        distance = length(R)
 
-                        if (distance < minimal_distance):
-                            minimal_distance = distance
-                            
-            # save the values for given i,j into the matrix:
-            minimal_distance_matrix[i,j] = minimal_distance
-            
-    return minimal_distance_matrix, species_vector
-
-def gengraph(minimal_distance_matrix, species_vector, ionic_radii, hypar=1.5):
+def gen_graph(material, graph_type, hypar=1.5):
     """Generates the crystal graph.
     
     Keyword arguments:
-    minimal_distance_matrix -- matrix of minimal distances
-    species_vector -- the names of the atoms in the unit cell
-    ionic_radii -- dictionary of ionic radii for the atoms spicies
+    material -- instance of IStructure
+    graph_type -- options: metal_oxygen_cons, all_cons, metal_metal_cons
     hypar -- hyperparameter which tunes the decision distances, default value is 1.5
     """
-    number_of_total_atoms = len(minimal_distance_matrix)
+    number_of_atoms = len(material.distance_matrix)
     
+    species_vector = [str(atom) for atom in material.species]    
+
+    # Shannon radii for coordination IV of O and coordination VI of Al, Ga, In
+
+    R_O = core.Element.O.data["Shannon radii"]["-2"]["II"][""]["ionic_radius"]
+    R_Al = core.Element.Al.data["Shannon radii"]["3"]["VI"][""]["ionic_radius"]
+    R_Ga = core.Element.Ga.data["Shannon radii"]["3"]["VI"][""]["ionic_radius"]
+    R_In = core.Element.In.data["Shannon radii"]["3"]["VI"][""]["ionic_radius"]
+
+    radii = { "O" : R_O, "Al" : R_Al, "Ga" : R_Ga, "In" : R_In }
+
     G = nx.Graph() # Empty graph.
     
-    for i in range(number_of_total_atoms):
+    metal_oxygen_cons = """
+if (namei == "O" and namej != "O") or (namei != "O" and namej == "O"):
+                
+    nodei = "".join([namei," ",str(i)])
+    nodej = "".join([namej," ",str(j)])
+                
+    # The decision:
+    if material.distance_matrix[i,j] < hypar * (radii[namei]+radii[namej]):
+        G.add_edge(nodei, nodej, distance=material.distance_matrix[i,j])
+"""
+
+    all_cons = """                
+nodei = "".join([namei," ",str(i)])
+nodej = "".join([namej," ",str(j)])
+                
+# The decision:
+if material.distance_matrix[i,j] < hypar * (radii[namei]+radii[namej]):
+    G.add_edge(nodei, nodej, distance=material.distance_matrix[i,j])
+"""
+
+    metal_metal_cons = """
+if (namei != "O" and namej != "O"):
+                
+    nodei = "".join([namei," ",str(i)])
+    nodej = "".join([namej," ",str(j)])
+                
+    # The decision:
+    if material.distance_matrix[i,j] < hypar * (radii[namei]+radii[namej]):
+        G.add_edge(nodei, nodej, distance=material.distance_matrix[i,j])
+"""
+
+    options = {
+            "metal_oxygen_cons": metal_oxygen_cons,
+            "all_cons": all_cons,
+            "metal_metal_cons": metal_metal_cons,
+            }
+
+
+    for i in range(number_of_atoms):
         namei = species_vector[i]
         for j in range(i):
             namej = species_vector[j]
             
-            # exclude oxygen connections:
-            if (namei == "O" and namej != "O") or (namei != "O" and namej == "O"):
-                
-                nodei = "".join([namei," ",str(i)])
-                nodej = "".join([namej," ",str(j)])
-                
-                # The decision:
-                if minimal_distance_matrix[i,j] < hypar * (ionic_radii[namei]+ionic_radii[namej]):
-                    G.add_edge(nodei,nodej)
+            exec(options[graph_type])
     return G
 
 def get_neighbors(G):
@@ -155,21 +157,15 @@ def get_neighbors(G):
         dic[node_labels[i]] = list(G[node_labels[i]]) # list(G.neighbors(node_labels[i]))
     return dic
 
-def get_hood(G):
-    """Calculates the least amount of inbetween atoms between two atoms.
-    
-    Keyword argumets:
+def get_coordinations(G):
+    """Get coordination numbers of every atom in the graph.
+
+    Keyword arguments:
     G -- the crystal graph.
     """
     atoms = G.number_of_nodes()
-    path_length_matrix = np.zeros((atoms,atoms), dtype = int)
-    node_labels = sorted(dict(G.nodes.items()).keys(), key = lambda x: int(x.split()[1]))
+    node_labels = sorted(dict(G.nodes.items()).keys(), key=lambda x: int(x.split()[1]))
+    dic = {}
     for i in range(atoms):
-        for j in range(i):
-            try:
-                path_length_matrix[i, j] = nx.shortest_path_length(G, node_labels[i], node_labels[j])
-            except nx.NetworkXNoPath:
-                print("There is no path between some atoms.")
-                print("Atoms:")
-                print(node_labels)
-    return path_length_matrix
+        dic[node_labels[i]] = len(list(G[node_labels[i]]))
+    return dic
